bf750a4b786be9d15222c9c5182e02c08968515c
Orkhan Hajizada <h.orkhan1907@gmail.com>
2019-03-29 22:32:53 +0400

    Facts/Feedbacks CRUD done

---
+++ Promed/Areas/Manage/Controllers/FeedbacksController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Promed.DAL;
+using Promed.Models;
+using Promed.Areas.Manage.Helpers;
+
+namespace Promed.Areas.Manage.Controllers
+{
+    public class FeedbacksController : Controller
+    {
+        private PromedContext db = new PromedContext();
+
+        // GET: Manage/Feedbacks
+        public ActionResult Index()
+        {
+            return View(db.Feedbacks.ToList());
+        }
+
+        // GET: Manage/Feedbacks/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Feedback feedback = db.Feedbacks.Find(id);
+            if (feedback == null)
+            {
+                return HttpNotFound();
+            }
+            return View(feedback);
+        }
+
+        // GET: Manage/Feedbacks/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Manage/Feedbacks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Create([Bind(Include = "Id,Fullname,Photo,Text,Date")] Feedback feedback, HttpPostedFileBase Photo)
+        {
+            if (Photo == null)
+            {
+                ModelState.AddModelError("Photo", "Please Select file");
+            }
+            else
+            {
+                feedback.Photo = FileManager.Upload(Photo);
+            }
+
+
+            if (ModelState.IsValid)
+            {
+                db.Feedbacks.Add(feedback);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(feedback);
+        }
+
+        // GET: Manage/Feedbacks/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Feedback feedback = db.Feedbacks.Find(id);
+            if (feedback == null)
+            {
+                return HttpNotFound();
+            }
+            return View(feedback);
+        }
+
+        // POST: Manage/Feedbacks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Edit([Bind(Include = "Id,Fullname,Photo,Text,Date")] Feedback feedback, HttpPostedFileBase Photo)
+        {
+            db.Entry(feedback).State = EntityState.Modified;
+
+            if (Photo == null)
+            {
+                db.Entry(feedback).Property(a => a.Photo).IsModified = false;
+            }
+            else
+            {
+                FileManager.Delete(feedback.Photo);
+
+                feedback.Photo = FileManager.Upload(Photo);
+            }
+            if (ModelState.IsValid)
+            {
+                db.Entry(feedback).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(feedback);
+        }
+
+        // GET: Manage/Feedbacks/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Feedback feedback = db.Feedbacks.Find(id);
+            if (feedback == null)
+            {
+                return HttpNotFound();
+            }
+            return View(feedback);
+        }
+
+        // POST: Manage/Feedbacks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Feedback feedback = db.Feedbacks.Find(id);
+            db.Feedbacks.Remove(feedback);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
